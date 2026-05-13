7e766b82181141d23b4336daa63fd0d93860ac31
Taha-tec <tahairfan05@yahoo.com>
2026-05-14 00:04:22 +0500

    cors changed

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,6 @@ const app = express();
 app.use(cors({ 
     origin: [
         'http://localhost:3000', 
-        'http://127.0.0.1:3000', 
         'https://financial-tracker-donation-manage.vercel.app' // ADD YOUR DEPLOYED URL HERE
     ], 
     credentials: true 
